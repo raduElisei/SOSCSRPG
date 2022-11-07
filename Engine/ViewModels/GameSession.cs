623af1f71fbac6730c45fcf0c02af6d38f57b733
Radu Elisei <phoenixcreates@yahoo.ro>
2022-11-07 16:50:41 +0200

    Add view model

---
+++ Engine/ViewModels/GameSession.cs
@@ -0,0 +1,25 @@
+﻿using Engine.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.ViewModels
+{
+    public class GameSession
+    {
+        public Player CurrentPlayer { get; set; }
+
+        public GameSession()
+        {
+            CurrentPlayer = new Player();
+            CurrentPlayer.Name = "Bob";
+            CurrentPlayer.HitPoints = 25;
+            CurrentPlayer.Level = 1;
+            CurrentPlayer.ExperiencePoints = 0;
+            CurrentPlayer.Gold = 0;
+            CurrentPlayer.ExperiencePoints = 0;
+        }
+    }
+}
